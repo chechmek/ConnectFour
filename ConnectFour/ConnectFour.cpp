ca57b8a5aa136c0ec80930925e5ac771fe6088b4
chechmek <romanklut89@gmail.com>
2022-10-30 22:03:39 +0100

    Testing an array bug

--- ConnectFour/ConnectFour.cpp
+++ ConnectFour/ConnectFour.cpp
@@ -30,12 +30,59 @@ class Game {
 	player<playfield> player2;
 	UI<playfield> ui;
 	bool isEnded;
-	void checkIfEnd(const playfield& field, char sign) {
-		// check if there are 4 connnected
-		isEnded = false;
-		if (isEnded) {
-			ui.show_victory_screen(sign);
+	bool checkIfConnected(const playfield& f, char sign) {
+		for (int i = 0; i < f.height; ++i) {
+			for (int j = 0; j < f.width; ++j) {
+				if (f.stoneat(i, j) == f.none)
+					continue;
+				
+				if (connectedRight(f, i, j, f.stoneat(i, j)))
+					return true;
+				if (connectedDown(f, i, j, f.stoneat(i, j)))
+					return true;
+				if (connectedDiagonal(f, i, j, f.stoneat(i, j)))
+					return true;
+			}
 		}
+		return false;
+	}
+	bool connectedRight(const playfield& f, int h, int w, char sign) {
+		for (int i = w; i < w + 4; ++i) {
+			if (f.stoneat(h, i) != sign)
+				return false;
+		}
+		return true;
+	}
+	bool connectedDown(const playfield& f, int h, int w, char sign) {
+		for (int i = h; i < h + 4; ++i) {
+			if (f.stoneat(i, h) != sign)
+				return false;
+		}
+		return true;
+	}
+	bool connectedDiagonal(const playfield& f, int h, int w, char sign) {
+		for (int i = 0; i < 4; ++i) {
+			if (f.stoneat(h + i, w + i) != sign)
+				return false;
+		}
+		return true;
+	}
+	void connectedTests() {
+		playfield testField1;
+		for (int i = 0; i < testField1.height; ++i) {
+			for (int j = 0; j < testField1.width; ++j) {
+				testField1.rep[i][j] = 0;
+			}
+		}
+		testField1.rep[5][0] = 1;
+		testField1.rep[5][1] = 1;
+		testField1.rep[5][2] = 1;
+		testField1.rep[5][3] = 1;
+		ui.show_field(testField1);
+
+		bool res = checkIfConnected(testField1, 1);
+
+		cout << res;
 	}
 	bool placeStone(int col, playfield& field, char sign) {
 		
@@ -56,12 +103,16 @@ class Game {
 		int col = player.play(field);
 		bool res = placeStone(col, field, signature);
 		ui.show_field(field);
-		checkIfEnd(field, signature);
+		
+		if (checkIfConnected(field, signature)) {
+			isEnded = true;
+			ui.show_victory_screen(signature);
+		}
 		return res;
 	}
 	void initField() {
-		for (int i = 0; i < field.height + 123; ++i) {
-			for (int j = 0; j < field.width + 123; ++j) {
+		for (int i = 0; i < field.height; ++i) {
+			for (int j = 0; j < field.width; ++j) {
 				field.rep[i][j] = 0;
 			}
 		}
@@ -73,7 +124,9 @@ public:
 	}
 	void Run() {
 		bool moved;
+		connectedTests();
 		ui.show_field(field);
+		
 		while (!isEnded) {
 			/*int a = 1;
 			field.rep[1][1] = 't';
@@ -83,12 +136,16 @@ public:
 			while (!moved)
 			{
 				moved = play(player1, field.player1);
+			cout << "field.height: " << field.height << endl;
 			}
 			moved = false;
 			while (!moved)
 			{
 				moved = play(player2, field.player2);
 			}
+			/*cout << field.stoneat(4, 6) << ", " << field.stoneat(5, 0) << endl;
+			field.rep[5][0] = 3;
+			cout << field.stoneat(4, 6) << ", " << field.stoneat(5,0) << endl;*/
 		}
 	}
 };
